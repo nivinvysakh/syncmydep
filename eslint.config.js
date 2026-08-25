0376145f657e8e152a03dedf7e47a779defdac44
nivin <nivinvysakh@icloud.com>
2026-08-25 15:13:27 +0530

    ci: fix windows dist diff false positive and clean up eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,11 @@ module.exports = tseslint.config(
   {
     files: ['src/**/*.ts', 'tests/**/*.ts'],
     rules: {
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off'
     }
   }
 );
+
